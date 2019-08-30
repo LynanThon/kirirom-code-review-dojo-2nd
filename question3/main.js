6b5ce02eb71677e57d93516716eeb2951295c062
LynanThon <thonlynan17@kit.edu.kh>
2019-08-30 16:53:30 +0700

    Dollar Riel Change

--- question3/main.js
+++ question3/main.js
@@ -1,3 +1,32 @@
 "use strict"
 
 //write code in here
+
+const cocacola = document.getElementById('cocaCola')
+const angkorbeer = document.getElementById('angkorBeer')
+const moringalunch = document.getElementById('moringaLunch')
+const book = document.getElementById('book')
+const purchaseName = document.getElementById('purchaseGoodsName')
+const purchasePrice = document.getElementById('purchaseGoodsPrice')
+const paymentInput = document.getElementById('paymentInput')
+const calButton = document.getElementById('calculateButton')
+const change = document.getElementById('calculatedChange')
+
+const PriceCalculation = (good, price) => () => {
+    purchaseName.textContent = good
+    purchasePrice.textContent = price
+    paymentInput.value = ''
+}
+
+function calculation() 
+{
+    let payment = parseInt(paymentInput.value)
+    let goodPrice =parseFloat(purchasePrice.textContent)
+    change.textContent = ((payment-goodPrice)*4091).toFixed(2)
+}
+
+book.addEventListener('click', PriceCalculation('Book', '4.5'))
+moringalunch.addEventListener('click', PriceCalculation('Moringa Lunch', '3.5'))
+angkorbeer.addEventListener('click', PriceCalculation('Angkor Beer', '2.25'))
+cocacola.addEventListener('click', PriceCalculation('Coca-cola', '1.15'))
+calButton.addEventListener('click', () => calculation())
