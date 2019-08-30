8e80b2de2f50893ef5d2c25b14151d30d1f46487
LynanThon <thonlynan17@kit.edu.kh>
2019-08-30 15:16:01 +0700

    calculation

--- question1/main.js
+++ question1/main.js
@@ -1,3 +1,24 @@
 "use strict"
 
 //write code in here
+const simpleCounter = document.getElementById('number')
+
+const plusNumber = document.getElementById('plus')
+const minusNumber = document.getElementById('minus')
+const resetNumber = document.getElementById('reset')
+
+let num = 0
+
+function Counter(number){
+    if (number === '+'){
+        simpleCounter.textContent = (++num)
+    } else if (number === '-'){
+        simpleCounter.textContent = (--num)
+    } else {
+        simpleCounter.textContent = '0'
+    }
+}
+
+plusNumber.addEventListener('click', () => Counter('+'))
+minusNumber.addEventListener('click', () => Counter('-'))
+resetNumber.addEventListener('click', () => Counter(''))
--- question2/main.js
+++ question2/main.js
@@ -1,3 +1,26 @@
 "use strict"
 
 //write code in here
+const priceInput = document.getElementById('priceInput')
+const taxRatioInput = document.getElementById('taxRatioInput')
+const calButton = document.getElementById('calculateButton')
+const resetButton = document.getElementById('resetButton')
+const taxPrice = document.getElementById('taxPrice')
+const taxInclude = document.getElementById('taxIncludedPrice')
+
+const taxCalculation = symbol => () => {
+    const price = parseInt(priceInput.value)
+    const taxRat = parseInt(taxRatioInput.value)
+    if (symbol === 'cal') {
+        taxPrice.textContent = Math.floor((price*taxRat)/100);
+        taxInclude.textContent = price + parseInt(taxPrice.textContent);
+    } else {
+        taxPrice.textContent = "0"
+        taxInclude.textContent = "0"
+        priceInput.value = ''
+        taxRatioInput.value = ''
+    }
+}
+
+calButton.addEventListener('click', taxCalculation('cal'))
+resetButton.addEventListener('click', taxCalculation(''))
